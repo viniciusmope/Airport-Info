88c8a5ff3adfe47e5e8d43130ab11d8761e83455
José Goulart <josevictor2000goulart@hotmail.com>
2024-02-21 20:11:26 -0300

    Adjust indentation, code pattern and simplify function

--- js/app.js
+++ js/app.js
@@ -1,25 +1,18 @@
-function mostrarHora() {
-    var dataAtual = new Date();
-    var horas = dataAtual.getHours();
-    var minutos = dataAtual.getMinutes();
-    var segundos = dataAtual.getSeconds();
-  
-    horas = formatarNumero(horas);
-    minutos = formatarNumero(minutos);
-    segundos = formatarNumero(segundos);
-  
-    document.getElementById('horaAtual').innerHTML = horas + ':' + minutos + ':' + segundos;
-  }
-  
-  function formatarNumero(numero) {
-    if (numero < 10) {
-      return '0' + numero;
-    }
-    return numero;
-  }
-  
+const displayTime = () => {
+  let currentTime = new Date();
+  let hours = currentTime.getHours();
+  let minutes = currentTime.getMinutes();
+  let seconds = currentTime.getSeconds();
 
-  setInterval(mostrarHora, 1000);
-  
-  mostrarHora();
-  
+  hours = doubleDigitNumber(hours);
+  minutes = doubleDigitNumber(minutes);
+  seconds = doubleDigitNumber(seconds);
+
+  document.getElementById('clock').innerHTML = hours + ':' + minutes + ':' + seconds;
+}
+
+const doubleDigitNumber = number => number < 10 ? '0' + number : number;
+
+displayTime();
+
+setInterval(displayTime, 1000);
